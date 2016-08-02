57d4bff2eefcbc55f2daa3c117f45d7c49dd4db3
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-08-02 15:26:38 +0700

    Add cli-server environment detection

--- index.php
+++ index.php
@@ -53,7 +53,8 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', php_sapi_name() == 'cli-server' ? 'cli-server' : 
+		(isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development'));
 
 /*
  *---------------------------------------------------------------
@@ -72,6 +73,7 @@ switch (ENVIRONMENT)
 
 	case 'testing':
 	case 'production':
+	case 'cli-server':
 		ini_set('display_errors', 0);
 		if (version_compare(PHP_VERSION, '5.3', '>='))
 		{
