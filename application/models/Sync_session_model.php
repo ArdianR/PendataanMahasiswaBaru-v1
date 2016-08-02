cdc2771136da989defe59ade24262d0a6c5e5697
Muhammad Saiful Islam <muhammad@saiful.web.id>
2016-08-02 20:53:34 +0700

    Create sync_session model

---
+++ application/models/Sync_session_model.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Sync_session_model extends CI_Model {
+	public $id;
+	public $start_time;
+	public $end_time;
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function get_all($tahun)
+	{
+		$query = $this->db->get('sync_session');
+		return $query->result();
+	}
+
+	public function create_new()
+	{
+		$id = md5(microtime());
+
+		$query = $db->query("INSERT INTO `sync_session` (`id`) VALUES (?)", array($id));
+
+		if ($query)
+		{
+			return $id;
+		}
+		else
+		{
+			return $query;
+		}
+	}
+
+	public function create_basic_schema($db = NULL)
+	{
+		if ($db === NULL)
+			$db = $this->db;
+
+		return $db->query("CREATE TABLE `sync_session` (
+							  `id` varchar(32) NOT NULL PRIMARY KEY,
+							  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+							  `end_time` timestamp
+							)");
+	}
+}
--- sql/2016-08-02_sync_mahasiswa.sql
+++ sql/2016-08-02_sync_mahasiswa.sql
@@ -1,3 +1,9 @@
 ALTER TABLE `mahasiswa` ADD `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `created_at`, ADD `synced_at` TIMESTAMP NULL AFTER `updated_at`;
 UPDATE `mahasiswa` SET `updated_at` = `created_at`;
 UPDATE `mahasiswa` SET `synced_at` = `created_at`;
+
+CREATE TABLE `sync_session` (
+  `id` varchar(32) NOT NULL PRIMARY KEY,
+  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `end_time` timestamp
+);
